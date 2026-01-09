cc3f9770e2161add39fe32c178c648edd60c927f
Ryan DeFalco <feircedoubleletter@gmail.com>
2026-01-09 18:21:19 -0500

    Added autocomplete support for number fields, style adjustments to the summary box

--- FrontEnd/front.js
+++ FrontEnd/front.js
@@ -23,6 +23,7 @@
   let summaryRVOL = document.getElementById("rvolBox")
   let summaryPercentUp = document.getElementById("percentUpBox")
   let summaryFloat = document.getElementById("floatBox")
+  let backgroundTicker = document.getElementById("backgroundTicker");
 
 
   function submit(){
@@ -30,7 +31,7 @@
       return
     getAllFields();
     sendToBackend(stockData); 
-    // clearAllFields(); 
+    clearAllFields(); 
     calculateSummary();
   }
 
@@ -64,6 +65,7 @@
     summaryRVOL.textContent = `The relative volume is ${RVOL}x above average`; 
     summaryPercentUp.textContent = `${stockData.symbol} is ${percentChange}% ${position} for the day`; 
     summaryFloat.textContent = `${stockData.float} Shares`; 
+    backgroundTicker.textContent = `${stockData.symbol}`;
   }
 
   function sendToBackend(data){
@@ -81,4 +83,68 @@
       console.log("Backend response:", data);
     })
     .catch(err => console.error("Error sending data:", err));
+  }
+
+  setInterval(update, 100);
+
+  function update(){
+    // Check if the numerical fields contain value indicators e.g. M (million), K (thousand), ect.
+    const fields = document.querySelectorAll('.entryFieldNumber');
+    fields.forEach((field, index) => {
+      if (field.value.toLowerCase().includes("k")){
+        field.value = field.value.replaceAll("k", ""); 
+        autoAdjust(field, 1000);
+        flashIndicateField(field);
+      }
+      else if (field.value.toLowerCase().includes("m")){
+        field.value = field.value.replaceAll("m", ""); 
+        autoAdjust(field, 1000000);
+        flashIndicateField(field);
+      }
+      else if (field.value.toLowerCase().includes("b")) {
+        field.value = field.value.replaceAll("b", ""); 
+        autoAdjust(field, 1000000000);
+        flashIndicateField(field);
+      }
+    })
+    // If so then auto adjust the field to the actual value fully written out e.g. 2.5M --> 2,500,000
+
+    function autoAdjust(field, number){
+      // Initiate factor and length to 0
+      let factor = 0; 
+      let length = 0; 
+      // If the field passed in includes a dot, then check how many numbers are behind the dot 
+      if (field.value.includes(".")) {
+        const splitNumber = field.value.split(".");
+        length = splitNumber[1].length; // Store that number here
+      }
+      console.log(`The length behind the dot: ${length}`);
+      // Check if the length is 1 or 2
+      switch (length) {
+        case 1: // if the length is 1 then divide by a factor of 10 once 
+          factor = (number / 10)
+          break
+        case 2:
+          factor = ((number / 10) / 10) // if the length is 2 then divide by a factor of 10 twice
+          break
+        default:
+          factor = number
+          break
+      }
+      // Remove the period from the string before math 
+      field.value = field.value.replaceAll(".", ""); 
+      // Adjust the number with the factor calculated ealier 
+      const changedNumber = Number(field.value) * factor;
+      // Set it back to a string and format the commas 
+      field.value = changedNumber.toLocaleString();
+      // Idicate to the user that the field was changed
+    }
+
+    function flashIndicateField(field){
+      field.style.color = "#02a02cff"; 
+      setTimeout(()=>{
+        field.style.transition = "color 0.5s ease-out"
+        field.style.color = "black"; 
+      }, 500)
+    }
   }
